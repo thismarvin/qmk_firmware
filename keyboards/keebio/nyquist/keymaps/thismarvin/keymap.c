6c4e51faa964f06876a45f4a6797c874562ba579
Marvin Valenzuela <me@thismarvin.com>
2023-07-30 20:22:53 -0500

    enable caps word
    Maybe I should just add a caps lock button...

--- keyboards/keebio/nyquist/keymaps/thismarvin/keymap.c
+++ keyboards/keebio/nyquist/keymaps/thismarvin/keymap.c
@@ -174,6 +174,40 @@ void leader_end_user(void) {
     }
 }
 
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS: {
+            // Apply shift to next key.
+            add_weak_mods(MOD_BIT(KC_LSFT));
+
+            return true;
+        }
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS: {
+            return true;
+        }
+
+        // I had to hack this in for Workman to work...
+        case KC_SCLN: {
+            // Apply shift to next key.
+            add_weak_mods(MOD_BIT(KC_LSFT));
+
+            return true;
+        }
+
+        // Deactivate Caps Word.
+        default: {
+            return false;
+        }
+    }
+}
+
 void persistent_default_layer_set(uint16_t default_layer) {
     eeconfig_update_default_layer(default_layer);
     default_layer_set(default_layer);
